4be58d5b131259b872cac5c7ffcbb704bb951b80
tranduydong2005-bit <tranduydong2005@gmail.com>
2025-10-29 00:24:12 +0700

    Remove .pyc files and update .gitignore

--- Back_end/app/core/config.py
+++
@@ -1,9 +0,0 @@
-from ast import Try
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-db_url = "mysql+pymysql://root:123456@localhost:3306/web_shop"
-
-engine = create_engine(db_url, echo = True)
-SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
-Base = declarative_base()
